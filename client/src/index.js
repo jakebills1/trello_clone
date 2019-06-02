f8b329904b5f660dfbb4b57f495fd383aa944ce5
Jake Bills <jakebills1@gmail.com>
2019-06-02 10:35:27 -0600

    adds fetchuser and protectedroute

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, } from 'react-router-dom';
 import { AuthProvider, } from './providers/AuthProvider';
+import { initMiddleWare, } from 'devise-axios';
+initMiddleWare();
 ReactDOM.render(
   <AuthProvider>
     <BrowserRouter>
